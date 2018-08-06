0bf87fbf530a7a0b7b2aee2dd2819b1a8512ec72
Hellesandro Gonzaga <hellesandro@hotmail.com>
2018-08-06 11:18:25 -0300

    Revisao

---
+++ RevisaoOO_ComercioEletronico/src/revisaooo_comercioeletronico/Pedido.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package revisaooo_comercioeletronico;
+
+/**
+ *
+ * @author 20161bsi0390
+ */
+public class Pedido {
+    private int qnt;
+    private double valorTotal;
+    
+}
---
+++ RevisaoOO_ComercioEletronico/src/revisaooo_comercioeletronico/Produto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package revisaooo_comercioeletronico;
+
+/**
+ *
+ * @author 20161bsi0390
+ */
+public class Produto {
+    private String categoria;
+    private String descricao; 
+    private double preco;
+    private int qntEstoque;
+}
